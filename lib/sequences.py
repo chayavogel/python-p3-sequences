7bad42fb8d1abba35d5f206a7c89bdb060192f1c
chaya <chayavogel1@gmail.com>
2023-11-15 14:53:27 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    if length == 0:
+        sequence = []
+    elif length == 1:
+        sequence = [0]
+    elif length == 2:
+        sequence = [0,1]
+    else:
+        sequence = [0,1,1]
+        i = 4
+        while i <= length:
+            sequence.append(sequence[-1] + sequence[-2])
+            i+=1
+
+    print(sequence)
+    return sequence
